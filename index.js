8a55cf0054ad01ee7366295bd6c7c73275a410b5
SadieF <sadiefreeman@gmail.com>
2019-04-18 10:05:35 -0700

    Scaffold up base resume

---
+++ index.js
@@ -0,0 +1,42 @@
+$(function () {
+    $('.me-link').on('click', function () {
+        $('.experience').fadeOut(250)
+        $('.skills').fadeOut(250)
+        $('.projects').fadeOut(250)
+        $(this).addClass('current')
+        $('.experience-link').removeClass('current')
+        $('.skills-link').removeClass('current')
+        $('.projects-link').removeClass('current')
+        $('.me').fadeIn(700)
+    });
+    $('.experience-link').on('click', function () {
+        $('.me').fadeOut(250)
+        $('.skills').fadeOut(250)
+        $('.projects').fadeOut(250)
+        $(this).addClass('current')
+        $('.me-link').removeClass('current')
+        $('.skills-link').removeClass('current')
+        $('.projects-link').removeClass('current')
+        $('.experience').fadeIn(700)
+    });
+    $('.skills-link').on('click', function () {
+        $('.me').fadeOut(250)
+        $('.experience').fadeOut(250)
+        $('.projects').fadeOut(250)
+        $(this).addClass('current')
+        $('.me-link').removeClass('current')
+        $('.experience-link').removeClass('current')
+        $('.projects-link').removeClass('current')
+        $('.skills').fadeIn(700)
+    });
+    $('.projects-link').on('click', function () {
+        $('.me').fadeOut(250)
+        $('.experience').fadeOut(250)
+        $('.skills').fadeOut(250)
+        $(this).addClass('current')
+        $('.me-link').removeClass('current')
+        $('.experience-link').removeClass('current')
+        $('.skills-link').removeClass('current')
+        $('.projects').fadeIn(700)
+    });
+});
